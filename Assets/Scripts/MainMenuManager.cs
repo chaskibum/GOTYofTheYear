f7923e568ffe4186541524366a84c4a6a30e2af5
Bruno Giacometti <152458112+chaskibum@users.noreply.github.com>
2025-09-22 00:52:51 -0300

    salto player arreglado, plataformas movibles
    sprite nuevo en las plataformas movibles, el player puede quedarse quieto al caer despues de moverse en el aire (en lugar de seguir hacia la ultima direccion que se movio)

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -6,7 +6,7 @@ public class MainMenuManager : MonoBehaviour
     public void Play()
     {
         Time.timeScale = 1;
-        SceneManager.LoadScene("PrototypeScene");
+        SceneManager.LoadScene("Alpha Test Level 1");
     }
 
     public void Exit()
--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -104,13 +104,15 @@ namespace PlayerScripts
 
         private void CheckMovementInput()
         {
-            // _xInput = Input.GetAxis("Horizontal");
-            if (Input.GetKey(KeyCode.A))
+            _xInput = Input.GetAxis("Horizontal");
+            /*if (Input.GetKey(KeyCode.A))
                 _xInput = -1f;
             else if (Input.GetKey(KeyCode.D))
                 _xInput = 1f;
             else
+            {
                 _xInput = 0f;
+            }*/
         }
 
         private void CheckAttackInput()
