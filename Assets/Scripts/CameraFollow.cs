ed607c688cbd045873984a6ecd852657315a4f77
Bruno Giacometti <152458112+chaskibum@users.noreply.github.com>
2025-10-20 13:46:46 -0300

    parallax en menu
    el menu se mueve en base a la posicion del mouse. falta agregar limites para que no se siga yendo.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,17 +1,49 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private float followSpeed = 2f;
     [SerializeField] private Transform target;
     [SerializeField] private float yOffset = 3f;
+    [SerializeField] private GameObject limits;
+    
+    private Camera _camera;
+    private Mouse _mouse;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+        _mouse = Mouse.current;
+    }
 
     private void LateUpdate()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
-        transform.position = Vector3.Lerp(transform.position, newPos, followSpeed * Time.deltaTime);
+        if (!target)
+        {
+            MoveBasedOnMousePosition();
+            CheckLimits();
+        }
+        else
+        {
+            Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
+            transform.position = Vector3.Lerp(transform.position, newPos, followSpeed * Time.deltaTime);
+        }
         
         MoveCamera();
+    }
+
+    private void MoveBasedOnMousePosition()
+    {
+        Vector3 newTarget = new Vector3(_mouse.position.ReadValue().x, _mouse.position.ReadValue().y, -10f);
+        Vector3 worldPosition = _camera.ScreenToWorldPoint(newTarget);
+        transform.position = Vector3.Lerp(transform.position, worldPosition, followSpeed * Time.deltaTime);
+    }
+
+    private void CheckLimits()
+    {
+        
     }
 
     private void MoveCamera()
